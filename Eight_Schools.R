5edbad76eec1533ebf1725f505867d1f1831e525
Sean Cranford <sean.a.cranford.ie@gmail.com>
2022-08-20 08:21:45 -0700

    Add R and Stan files

---
+++ Eight_Schools.R
@@ -0,0 +1,20 @@
+
+##From the rStan vignette: https://cran.r-project.org/web/packages/rstan/vignettes/rstan.html#example
+
+##Initiate dataset
+schools_data <- list(
+  J = 8,
+  y = c(28,  8, -3,  7, -1,  1, 18, 12),
+  sigma = c(15, 10, 16, 11,  9, 11, 10, 18)
+)
+
+library(rstan)
+fit1 <- stan(
+  file = "schools.stan",  # Stan program
+  data = schools_data,    # named list of data
+  chains = 4,             # number of Markov chains
+  warmup = 1000,          # number of warmup iterations per chain
+  iter = 2000,            # total number of iterations per chain
+  cores = 1,              # number of cores (could use one per chain)
+  refresh = 0             # no progress shown
+)
